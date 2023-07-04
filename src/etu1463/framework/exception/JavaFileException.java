69d88b34d466b54afe018065a9b53a026014d308
bal2203 <117754805+bal2203@users.noreply.github.com>
2023-07-04 08:56:42 +0300

    commit3

---
+++ src/etu1463/framework/exception/JavaFileException.java
@@ -0,0 +1,7 @@
+package etu1463.framework.exception;
+
+public class JavaFileException extends Exception {
+    public JavaFileException() {
+        super("ERROR: This is not a java file");
+    }
+}
---
+++ src/etu1463/framework/servlet/FrontServlet.java
@@ -0,0 +1,94 @@
+package etu1463.framework.servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+
+import etu1463.framework.Mapping;
+import etu1463.framework.annotation.Url;
+import etu1463.framework.exception.JavaFileException;
+import etu1463.framework.javaObject.JavaClass;
+import etu1463.framework.javaObject.JavaFile;
+import fileActivity.Executor;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrontServlet extends HttpServlet {
+
+    private HashMap<String, Mapping> mappingUrls;
+
+    // setter
+    public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
+        this.mappingUrls = mappingUrls;
+    }
+
+    // getter
+    public HashMap<String, Mapping> getMappingUrls() {
+        return this.mappingUrls;
+    }
+
+    // methods
+    @Override
+    public void init() throws ServletException {
+        try {
+            this.setMappingUrls(new HashMap<String, Mapping>());
+            String rootPath = "/opt/apache-tomcat-10/webapps/springy/WEB-INF/classes/";
+            File root = new File(rootPath);
+            File[] fileTree = this.scanProject(root);
+            this.findAllMappedMethod(rootPath, fileTree);
+        } catch (ServletException e) {
+            throw e;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processingRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processingRequest(req, resp);
+    }
+
+    private void processingRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter out = resp.getWriter();
+        for (Map.Entry<String, Mapping> entry : this.getMappingUrls().entrySet()) {
+            out.println("For : \"" + entry.getKey() + "\"");
+            out.println("\tClass:" + entry.getValue().getClassName());
+            out.println("\tMethod:" + entry.getValue().getMethod());
+            out.println("\n\n\n");
+        }
+    }
+
+    private File[] scanProject(File root) throws Exception {
+        return Executor.getFileTree(root);
+    }
+
+    private void findAllMappedMethod(String rootPath, File[] fileTree) {
+        JavaFile javaFile = new JavaFile();
+        JavaClass javaClass = new JavaClass();
+        Mapping mapping = null;
+        for (File file : fileTree) {
+            try {
+                javaFile.setJavaFile(file);
+                javaClass.setJavaClass(javaFile.getClassObject(rootPath));
+
+                for (Method method : javaClass.getMethodByAnnotation(Url.class)) {
+                    mapping = new Mapping(javaClass.getJavaClass().getName(), method.getName());
+                    this.getMappingUrls().put(method.getAnnotation(Url.class).value(), mapping);
+                }
+            } catch (JavaFileException e) {
+                continue;
+            }
+        }
+    }
+}
