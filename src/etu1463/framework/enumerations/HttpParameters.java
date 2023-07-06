ad27c325e40744504d06e4030f52f78300ec02bd
bal2203 <117754805+bal2203@users.noreply.github.com>
2023-07-06 13:58:53 +0300

    commit10

---
+++ src/etu1463/framework/enumerations/HttpParameters.java
@@ -0,0 +1,5 @@
+package etu1463.framework.enumerations;
+
+public enum HttpParameters {
+    PATH_VARIABLE, REQUEST_PARAMETER
+}
---
+++ src/etu1463/framework/utils/javaObject/JavaFile.java
@@ -0,0 +1,45 @@
+package etu1463.framework.utils.javaObject;
+
+import java.io.File;
+
+import etu1463.framework.exceptions.JavaFileException;
+
+public class JavaFile {
+
+    private File javaFile;
+
+    // constructor
+    public JavaFile() {
+    }
+
+    public JavaFile(File file) throws Exception {
+        this.setJavaFile(file);
+    }
+
+    // setter
+    public void setJavaFile(File file) throws JavaFileException {
+        if (!isJavaFile(file))
+            throw new JavaFileException();
+        this.javaFile = file;
+    }
+
+    // getter
+    public File getJavaFile() {
+        return this.javaFile;
+    }
+
+    // methods
+    public boolean isJavaFile(File file) {
+        return file.getName().toLowerCase().endsWith(".java") || file.getName().toLowerCase().endsWith(".class");
+    }
+
+    public Class<?> getClassObject(String pathToRemove) {
+        String path = this.getJavaFile().getPath().replace(pathToRemove, "").replace("/", ".");
+        try {
+            return Class.forName(path.substring(0, path.length() - 6));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
